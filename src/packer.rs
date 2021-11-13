3964a496c3f528f81995d7ab443e7230f9ddba71
Ivan Temchenko <35359595i@gmail.com>
2021-11-13 23:25:55 +0100

    wallet failes with same controller

--- src/packer.rs
+++ src/packer.rs
@@ -7,38 +7,28 @@ use universal_wallet::{
 impl Iml {
     pub fn new(wallet: &mut UnlockedWallet) -> Self {
         let current_sk_raw = wallet
-            .new_key(
-                KeyType::EcdsaSecp256k1VerificationKey2019,
-                Some(vec!["current_sk".into()]),
-            )
+            .new_key(KeyType::EcdsaSecp256k1VerificationKey2019, None)
             .unwrap();
         let next_sk_raw = wallet
-            .new_key(
-                KeyType::EcdsaSecp256k1VerificationKey2019,
-                Some(vec!["next_sk".into()]),
-            )
+            .new_key(KeyType::EcdsaSecp256k1VerificationKey2019, None)
             .unwrap();
         let current_dh_raw = wallet
-            .new_key(
-                KeyType::X25519KeyAgreementKey2019,
-                Some(vec!["current_dh".into()]),
-            )
+            .new_key(KeyType::X25519KeyAgreementKey2019, None)
             .unwrap();
         let next_dh_raw = wallet
-            .new_key(
-                KeyType::X25519KeyAgreementKey2019,
-                Some(vec!["next_dh".into()]),
-            )
+            .new_key(KeyType::X25519KeyAgreementKey2019, None)
             .unwrap();
         let current_sk = get_pk_bytes(current_sk_raw.content);
         let next_sk = get_pk_bytes(next_sk_raw.content);
         let current_dh = get_pk_bytes(current_dh_raw.content);
         let next_dh = get_pk_bytes(next_dh_raw.content);
         let id = blake3::hash(&current_sk).to_string();
-        wallet.set_key_controller(&current_sk_raw.id, &format!("{}_current_sk", &id));
-        wallet.set_key_controller(&next_sk_raw.id, &format!("{}_next_sk", &id));
-        wallet.set_key_controller(&current_dh_raw.id, &format!("{}_current_dh", &id));
-        wallet.set_key_controller(&next_dh_raw.id, &format!("{}_next_dh", &id));
+        wallet.set_key_controller(&current_sk_raw.id, &format!("{}_sk_0", &id));
+        let next_sk_controller = &format!("{}_sk_1", &id);
+        println!("{}", &next_sk_controller);
+        wallet.set_key_controller(&next_sk_raw.id, next_sk_controller);
+        wallet.set_key_controller(&current_dh_raw.id, &format!("{}_dh_0", &id));
+        wallet.set_key_controller(&next_dh_raw.id, &format!("{}_dh_1", &id));
         let mut pre_signed = Iml {
             id,
             current_sk,
@@ -53,6 +43,7 @@ impl Iml {
         pre_signed.proof = Some(sig);
         pre_signed
     }
+
     pub fn evolve(
         self,
         wallet: &mut UnlockedWallet,
@@ -64,33 +55,45 @@ impl Iml {
             return self;
         }
         let mut evolved = Iml::default();
+        evolved.civilization = self.get_civilization() + 1;
+        evolved.id = self.id.clone();
         if evolve_sk {
-            let new_current = wallet.get_key(&format!("{}_next_sk", &self.id)).unwrap();
-            wallet.set_key_controller(&new_current.id, &format!("{}_current_sk", &self.id));
+            let current_controller = format!("{}_sk_{}", &self.id, evolved.get_civilization());
+            println!("{}", &current_controller);
+            let new_current = wallet
+                .get_content_by_controller(&current_controller)
+                .unwrap()
+                .clone();
             let next_sk_raw = wallet
                 .new_key(
                     KeyType::EcdsaSecp256k1VerificationKey2019,
-                    Some(vec!["next_sk".into()]),
+                    Some(vec![format!(
+                        "{}_sk_{}",
+                        &self.id,
+                        evolved.get_civilization() + 1
+                    )]),
                 )
                 .unwrap();
-            wallet.set_key_controller(&next_sk_raw.id, &format!("{}_next_sk", &self.id));
-            evolved.current_sk = get_pk_bytes(new_current.content);
+            evolved.current_sk = get_pk_bytes(new_current);
             evolved.next_sk = get_pk_bytes(next_sk_raw.content);
         }
         if evolve_dh {
-            let new_current = wallet.get_key(&format!("{}_next_dh", &self.id)).unwrap();
-            wallet.set_key_controller(&new_current.id, &format!("{}_current_dh", &self.id));
+            let new_current = wallet
+                .get_key_by_controller(&format!("{}_dh_{}", &self.id, evolved.get_civilization()))
+                .unwrap();
             let next_dh_raw = wallet
                 .new_key(
                     KeyType::X25519KeyAgreementKey2019,
-                    Some(vec!["next_dh".into()]),
+                    Some(vec![format!(
+                        "{}_dh_{}",
+                        &self.id,
+                        evolved.get_civilization() + 1
+                    )]),
                 )
                 .unwrap();
-            wallet.set_key_controller(&next_dh_raw.id, &format!("{}_next_dh", &self.id));
             evolved.current_dh = get_pk_bytes(new_current.content);
             evolved.next_dh = get_pk_bytes(next_dh_raw.content);
         }
-        evolved.civilization = self.get_civilization() + 1;
         evolved.inversion = serde_cbor::to_vec(&self).unwrap();
         let proof = wallet
             .sign_raw(
@@ -116,5 +119,8 @@ fn new_iml_plus_verification_test() {
     let mut wallet = UnlockedWallet::new("test");
     let iml = Iml::new(&mut wallet);
     assert_eq!(0, iml.get_civilization());
+    assert!(iml.verify());
+    let iml = iml.evolve(&mut wallet, true, false, None);
+    assert_eq!(1, iml.get_civilization());
     assert!(iml.verify());
 }
